2c662451c32a3d1beb5ecd38198d8b3e636556e8
Victor <vc.anokwuru@gmail.com>
2024-01-02 05:51:42 -0800

    mandatory tasks commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements in a linked listint_t list
+ * @h: the listint_t list
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t elem_count = 0;
+
+	while (h)
+	{
+		if (h->n)
+			elem_count++;
+
+		h = h->next;
+	}
+
+	return (elem_count);
+}
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	/*unsigned int i;*/
+	const listint_t *current, *tmp;
+
+	if (list == NULL)
+		return (-1);
+	/*n = listint_len(list);*/
+	current = list;
+	tmp = list;
+	/*i = 0;*/
+	while (current != NULL)
+	{
+		current = current->next;
+		if (current == tmp)
+			return (1);
+	}
+	return (0);
+}
