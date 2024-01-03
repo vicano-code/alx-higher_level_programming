4451632885a419065dca5d87dfab68480d7d810e
Victor <vc.anokwuru@gmail.com>
2024-01-02 19:28:42 -0800

    code correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
-    if chr(char) != 'q' and chr(char) != 'e':
+    if char is not (ord('q')) and char is not (ord('e')):
         print(f"{chr(char)}", end='')
