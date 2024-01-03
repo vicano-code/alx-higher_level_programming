2a53d2acbf8a5f3f5cc760bbaa1bbe744ee4d387
Victor <vc.anokwuru@gmail.com>
2024-01-03 03:21:54 -0800

    code correction

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    ans = 1
-    for _ in range(abs(b)):
-        if b >= 0:
-            ans *= a
-        else:
-            ans /= a
-    return ans
+    return a**b
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        if tens_digit < 9 or ones_digit < 8:
-            print("{}{}".format(tens_digit, ones_digit), end=", ")
+for m in range(0, 10):
+    for n in range(m + 1, 10):
+        if m < 8 and n < 10:
+            print("{}{}".format(m, n), end=", ")
         else:
-            print("\n")
+            print("{}{}\n".format(m, n), end="")
