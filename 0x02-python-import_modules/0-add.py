ce3a9b18cba264079285da5046c41138ba816072
Victor <vc.anokwuru@gmail.com>
2024-01-04 05:11:14 -0800

    update for import module execution prevention

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as ad
+if __name__ == "__main__":
+    import add_0 as ad
 
-a = 1
-b = 2
-result = ad.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, ad.add(a, b)))
