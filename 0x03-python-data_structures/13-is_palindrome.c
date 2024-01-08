3d34113af37a0603a13beb01680094816ed74294
Victor <vc.anokwuru@gmail.com>
2024-01-08 07:52:15 -0800

    code review

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,8 +11,13 @@ int is_palindrome(listint_t **head)
 	int i, a;
 	listint_t *current;
 
-	if (*head == NULL)
+	if (head == NULL)/*No list*/
+		return (0);
+	if (*head == NULL)/*empty list*/
 		return (1);
+	if (*head != NULL && *head->next == NULL)/*single node list*/
+		return (1);
+
 	current = *head;
 	arr[0] = current->n;
 	i = 0;
