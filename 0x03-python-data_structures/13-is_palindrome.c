cee37909adc90e810a65108f29146cae3527c2cb
Victor <vc.anokwuru@gmail.com>
2024-01-08 07:30:24 -0800

    new task-palindrome commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the pointer of the list
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int arr[1024];
+	int i, a;
+	listint_t *current;
+
+	if (*head == NULL)
+		return (1);
+	current = *head;
+	arr[0] = current->n;
+	i = 0;
+	while (current->next != NULL)
+	{
+		current = current->next;
+		i++;
+		arr[i] = current->n;
+	}
+	a = 0;
+	while (a <= i)
+	{
+		if (arr[a] != arr[i])
+			return (0);
+		a++;
+		i--;
+	}
+	return (1);
+}
