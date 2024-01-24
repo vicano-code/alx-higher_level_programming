3941a57e299eaa443a2179a57fa4d132b82c79bb
Victor <vc.anokwuru@gmail.com>
2024-01-24 15:09:38 -0800

    syntax check

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -18,11 +18,11 @@ class MagicClass:
 
         def area(self):
             """Calculate area"""
-            return (self.__radius**2) * math.pi
+            return self.__radius ** 2 * math.pi
 
         def circumference(self):
             """Calculate circumference"""
-            return (2 * math.pi * self.__radius)
+            return 2 * math.pi * self.__radius
 
 
 # import dis
