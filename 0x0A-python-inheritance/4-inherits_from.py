de8ce7d8a4a8c499fa0114ea8152b9968c8608d7
Victor <vc.anokwuru@gmail.com>
2024-02-07 01:11:17 -0800

    remove from from comments (given instruction)

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Module 4-inherits_from
+Module 4-inherits_from.py
 
 Contains  a function that returns True if the object is an
 instance of a class that inherited (directly or indirectly)
-from the specified class, otherwise False
+frm the specified class, otherwise False
 """
 
 
 def inherits_from(obj, a_class):
     """
     checks if an object is an instance of a class that inherited
-    (directly or indirectly) from the specified class
+    (directly or indirectly) frm the specified class
     """
-    return type(obj) is not  a_class
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
