4c9fa0d0c9658e6e744e0e3bdf84d0df09dcacf4
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-02-12 08:50:34 -0800

    tests update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,26 +2,20 @@
 """
 Module Square
 
-
+Inherits from Rectangle class
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
+    class square with methods
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """initialize square attributes from superclass rectangle"""
         super().__init__(size, size, x, y, id)
-        self.__size = size
-        self.__x = x
-        self.__y = y
-
-    def __str__(self):
-        """override the in-built str method"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
-                self.id, self.__x, self.__y, self.__size)
+        self.size = size
 
     @property
     def size(self):
@@ -37,27 +31,33 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.__size = value
 
+    def __str__(self):
+        """override the in-built str method"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+                self.__class__.__name__, self.id, self.x,
+                self.y, self.size)
+
     def update(self, *args, **kwargs):
         """updates the square attributes"""
         if args:
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.__size = args[1]
+                self.size = args[1]
             if len(args) >= 3:
-                self.__x = args[2]
+                self.x = args[2]
             if len(args) == 4:
-                self.__y = args[3]
+                self.y = args[3]
         else:
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
                 elif key == "size":
-                    self.__size = value
+                    self.size = value
                 elif key == "x":
-                    self.__x = value
+                    self.x = value
                 elif key == "y":
-                    self.__y = value
+                    self.y = value
 
     def to_dictionary(self):
         """returns the dictionary representation of a Square"""
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,11 +1,64 @@
 #!/usr/bin/python3
 
-"""unittest for Base class"""
+"""
+unittest for Base class
 
+# run with python3 -m unittest discover tests
+# run with python3 -m unittest tests/test_models/test_base.py
+"""
+
+
+import os
 import unittest
-from base.py import Base
+from io import StringIO
+from contextlib import redirect_stdout
+from models import base
+Base = base.Base
+from models import rectangle
+Rectangle = rectangle.Rectangle
+
 
 class TestBase(unittest.TestCase):
+    """
+    Base class tests
+    """
 
-    def test_base(self):
+    """Test attributes"""
+    def test_id_given(self):
+        """Test ids match"""
+        self.assertTrue(Base(99), self.id == 99)
+        self.assertTrue(Base(1), self.id == 1)
+        self.assertTrue(Base(0), self.id == 0)
+        self.assertTrue(Base(-20), self.id == -20)
 
+    def test_no_id_given(self):
+        """Test ids match nb_objects increment when not given"""
+        self.assertTrue(Base(), self.id == 1)
+        self.assertTrue(Base(), self.id == 2)
+
+    def test_private_attr_access(self):
+        """Test private attr are not accessible"""
+        with self.assertRaises(AttributeError):
+            print(Base.__nb_objects)
+            print(Base.nb_objects)
+
+    """Test args given"""
+    def test_invalid_args(self):
+        """Test too many args given"""
+        with self.assertRaises(TypeError):
+            Base(10, 10)
+
+    """Test class"""
+    def test_class(self):
+        """Test class created is indeed Base"""
+        self.assertTrue(Base(10), self.__class__ == Base)
+
+    def test_to_json_string(self):
+        """to_json_string method test"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(type(dictionary) == dict)
+        self.assertTrue(type(json_dictionary) == str)
+        self.assertTrue(json_dictionary, [{"x": 2, "width": 10,
+                                           "id": 3, "height": 7, "y": 8}])
