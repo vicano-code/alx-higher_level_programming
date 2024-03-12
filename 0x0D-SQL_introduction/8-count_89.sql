6d9d09cf06456eaa2d7bd0ad105274d2da15f8ce
Victor <vc.anokwuru@gmail.com>
2024-03-12 15:32:21 -0700

    correction

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,5 @@
 -- displays the number of records with id = 89 in the table first_table
 -- cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
 
-SELECT COUNT(id) FROM first_table where id = 89;
+SELECT COUNT(*) FROM first_table where id=89;
+
