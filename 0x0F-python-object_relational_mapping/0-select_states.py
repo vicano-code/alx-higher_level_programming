09f4413c1a333ed1c519359d099f742b89f2f04b
Victor <vc.anokwuru@gmail.com>
2024-04-15 15:00:21 -0700

    remove charset

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+'''
+ lists all states from the database hbtn_0e_0_usa
+'''
 import sys
 import MySQLdb
 
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                       passwd=sys.argv[2], db=sys.argv[3])
 cur = conn.cursor()
 cur.execute("SELECT * FROM states")
 query_rows = cur.fetchall()
