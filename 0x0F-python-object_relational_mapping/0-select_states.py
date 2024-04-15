95e06d708a79023119c871143f0326782bde1af9
Victor <vc.anokwuru@gmail.com>
2024-04-15 15:09:58 -0700

    PEP8 style correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 if __name__ == "__main__":
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3])
+                           passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states")
     query_rows = cur.fetchall()
