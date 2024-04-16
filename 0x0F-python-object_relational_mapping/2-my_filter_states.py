b89aa1907f07fc206a66a57680aba58653073017
Victor <vc.anokwuru@gmail.com>
2024-04-15 17:26:33 -0700

    PEP8 style correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,12 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(sys.argv[4]))
+    cmd = """SELECT * FROM states
+             WHERE name LIKE '{:s}' ORDER BY id ASC""".format(sys.argv[4])
+    cur.execute(cmd)
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
     cur.close()
     conn.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,11 +10,14 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities
-                INNER JOIN states ON cities.state_id=states.id
-                WHERE states.name=%s", (sys.argv[4],))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+    cmd = """SELECT cities.name
+             FROM cities
+             INNER JOIN states ON cities.state_id=states.id
+             WHERE states.name LIKE %s
+             ORDER BY cities.id ASC"""
+    cur.execute(cmd, (sys.argv[4],))
+
+    print(', '.join(["{:s}".format(row[0]) for row in cur.fetchall()]))
+
     cur.close()
     conn.close()
