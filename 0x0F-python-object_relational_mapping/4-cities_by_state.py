a20b875f5fbf4b71169051a66000933140ccf2a2
Victor <vc.anokwuru@gmail.com>
2024-04-15 17:10:47 -0700

    PEP8 style correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM states
-                INNER JOIN cities ON states.id=cities.state_id
-                ORDER BY cities.id ASC")
+    cmd = """SELECT cities.id, cities.name, states.name
+           FROM states
+           INNER JOIN cities ON states.id=cities.state_id
+           ORDER BY cities.id ASC"""
+    cur.execute(cmd)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
