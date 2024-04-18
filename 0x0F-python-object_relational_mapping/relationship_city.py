f61eb3d00f3f50d8d176c22287dce765361073b4
vicano-code <vc.anokwuru@gmail.com>
2024-04-18 13:45:30 -0700

    new advanced task 102

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,6 +11,6 @@ from relationship_state import Base
 
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True) #autoincrements
+    id = Column(Integer, primary_key=True)  # autoincrements
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
