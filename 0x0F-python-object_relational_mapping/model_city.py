3b55a2bb530ee8251a88a1f09755929ef39e5f80
vicano-code <vc.anokwuru@gmail.com>
2024-04-18 13:59:30 -0700

    pycodestyle correction

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,8 +10,9 @@ from model_state import Base, State
 
 Base = declarative_base()
 
+
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)  # autoincrements
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
