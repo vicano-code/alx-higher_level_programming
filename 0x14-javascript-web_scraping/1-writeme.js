46727edb18d5537498e9efc1f1884027a188eadb
vicano-code <vc.anokwuru@gmail.com>
2024-05-21 13:57:10 -0700

    variable definition

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let filePath = process.argv[2];
-let text = process.argv[3];
+const filePath = process.argv[2];
+const text = process.argv[3];
 const fs = require('fs');
 fs.writeFile(filePath, text, 'utf-8', (err) => {
   if (err) {
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
+const url = process.argv[2];
 const request = require('request');
 request.get(url, function (err, response) {
   if (err) {
